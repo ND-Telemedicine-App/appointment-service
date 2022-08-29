197965e237a0ebf4f071e741d7c73db2567623c6
doanyennhi <s3880599@rmit.edu.vn>
2022-08-29 22:22:55 +1000

    Add README, setup initial structure

---
+++ src/main/java/com/team4/appointment_service/AppointmentController.java
@@ -0,0 +1,7 @@
+package com.team4.appointment_service;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AppointmentController {
+}
---
+++ src/main/java/com/team4/appointment_service/AppointmentRepo.java
@@ -0,0 +1,4 @@
+package com.team4.appointment_service;
+
+public class AppointmentRepo {
+}
