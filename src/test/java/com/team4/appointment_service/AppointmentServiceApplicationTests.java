c4fff46ff8b77fc0912957d933e82488515c3c91
doanyennhi <s3880599@rmit.edu.vn>
2022-08-29 23:25:00 +1000

    Try unit testing

--- src/test/java/com/team4/appointment_service/AppointmentServiceApplicationTests.java
+++ src/test/java/com/team4/appointment_service/AppointmentServiceApplicationTests.java
@@ -1,13 +1,24 @@
 package com.team4.appointment_service;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.Assert;
+
 
 @SpringBootTest
 class AppointmentServiceApplicationTests {
 
+    @Autowired
+    private AppointmentService service;
+
     @Test
-    void contextLoads() {
+    public void whenContextLoads_thenServiceISNotNull() {
+
+        Assertions.assertNotNull(service);
+
+
     }
 
 }
