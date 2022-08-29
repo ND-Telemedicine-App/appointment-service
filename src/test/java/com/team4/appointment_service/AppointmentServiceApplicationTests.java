c5fe24f3b54656dd6573419ded269a0807886e22
doanyennhi <s3880599@rmit.edu.vn>
2022-08-29 21:54:27 +1000

    Initial setup

---
+++ src/test/java/com/team4/appointment_service/AppointmentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team4.appointment_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppointmentServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
