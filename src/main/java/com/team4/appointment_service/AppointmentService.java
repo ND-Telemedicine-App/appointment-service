eb841d01e2c7bd0934c2bc8a189852fb2ca6487b
Matt Rigo <mattrigo2872@gmail.com>
2022-09-02 21:47:10 +1000

    Added return functions to appointment service java file

--- src/main/java/com/team4/appointment_service/AppointmentService.java
+++ src/main/java/com/team4/appointment_service/AppointmentService.java
@@ -2,6 +2,28 @@ package com.team4.appointment_service;
 
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class AppointmentService {
+
+    private final AppointmentRepo appointmentRepository;
+
+
+    public AppointmentService(AppointmentRepo appointmentRepository) {
+        this.appointmentRepository = appointmentRepository;
+    }
+
+    public Appointment findById(Long id) {
+        return appointmentRepository.findAppointmentById(id);
+    }
+
+    public List<Appointment> findByPatientId(Long patientId) {
+        return appointmentRepository.findAppointmentsByPatientId(patientId);
+    }
+
+    public List<Appointment> findByDoctorId(Long doctorId) {
+        return appointmentRepository.findAppointmentsByDoctorId(doctorId);
+    }
+
 }
