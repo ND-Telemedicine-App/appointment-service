51e7651806641885a51184869b46d88980615759
Matt Rigo <mattrigo2872@gmail.com>
2022-09-02 21:53:23 +1000

    Added function in appointment controller to search database for certain data such as an appointment can be searched by it's unique id

--- src/main/java/com/team4/appointment_service/AppointmentController.java
+++ src/main/java/com/team4/appointment_service/AppointmentController.java
@@ -1,7 +1,43 @@
 package com.team4.appointment_service;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class AppointmentController {
+
+    private final AppointmentService appointmentService;
+
+
+    public AppointmentController(AppointmentService appointmentService) {
+        this.appointmentService = appointmentService;
+    }
+
+
+    //shows a specific appointment
+    @GetMapping(value = "/{id}")
+    public Appointment viewAppointmentById(@PathVariable Long id){
+        return appointmentService.findById(id);
+    }
+
+
+    //shows all appointments a patient has on record
+    @GetMapping(value = "/{patientId}")
+    public List<Appointment> viewAppointmentOfPatient(@PathVariable Long patientId){
+        return appointmentService.findByPatientId(patientId);
+    }
+
+
+    //shows all appointments a doctor has on record
+    @GetMapping(value = "/{doctorId}")
+    public List<Appointment> viewAppointmentOfDoctor(@PathVariable Long doctorId){
+        return appointmentService.findByDoctorId(doctorId);
+    }
+
+
+
+
 }
