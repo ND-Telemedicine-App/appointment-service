2feb1e4204fb1bff1ab830de99ae7f06d182b228
Matt Rigo <mattrigo2872@gmail.com>
2022-09-02 21:40:59 +1000

    Added functions to appointment repository

--- src/main/java/com/team4/appointment_service/AppointmentRepo.java
+++ src/main/java/com/team4/appointment_service/AppointmentRepo.java
@@ -1,4 +1,17 @@
 package com.team4.appointment_service;
 
-public class AppointmentRepo {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface AppointmentRepo extends JpaRepository<Appointment, Long> {
+
+    Appointment findAppointmentById(Long id);
+
+    List<Appointment> findAppointmentsByPatientId(Long patientId);
+
+    List<Appointment> findAppointmentsByDoctorId(Long doctorId);
+
+
+
 }
