567540da3250134651b97faeae652d6068175ed1
MingkaiWang-s3773115 <S3773115@student.rmit.edu.au>
2022-09-05 16:10:06 +1000

    finished 'deleteAppointmentById' method

--- src/main/java/com/team4/appointment_service/Appointment.java
+++ src/main/java/com/team4/appointment_service/Appointment.java
@@ -1,4 +1,92 @@
 package com.team4.appointment_service;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import javax.persistence.*;
+import java.sql.Date;
+
+@Entity
+@Table(name = "appointments")
 public class Appointment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "patientId")
+    private Long patientId;
+
+    @Column(name = "doctorId")
+    private Long doctorId;
+
+    @Column(name = "appointmentDate")
+    @JsonFormat(pattern="dd-MM-yyyy")
+    private Date appointmentDate;
+
+    @Column(name = "appointmentTime")
+    private String appointmentTime;
+
+    @Column(name = "appointmentDescription")
+    private String appointmentDescription;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getPatientId() {
+        return patientId;
+    }
+
+    public void setPatientId(Long patientId) {
+        this.patientId = patientId;
+    }
+
+    public Long getDoctorId() {
+        return doctorId;
+    }
+
+    public void setDoctorId(Long doctorId) {
+        this.doctorId = doctorId;
+    }
+
+    public Date getAppointmentDate() {
+        return appointmentDate;
+    }
+
+    public void setAppointmentDate(Date appointmentDate) {
+        this.appointmentDate = appointmentDate;
+    }
+
+    public String getAppointmentTime() {
+        return appointmentTime;
+    }
+
+    public void setAppointmentTime(String appointmentTime) {
+        this.appointmentTime = appointmentTime;
+    }
+
+    public String getAppointmentDescription() {
+        return appointmentDescription;
+    }
+
+    public void setAppointmentDescription(String appointmentDescription) {
+        this.appointmentDescription = appointmentDescription;
+    }
+
+    @Override
+    public String toString() {
+        return "Appointment{" +
+                "id=" + id +
+                ", patientId=" + patientId +
+                ", doctorId=" + doctorId +
+                ", appointmentDate=" + appointmentDate +
+                ", appointmentTime='" + appointmentTime + '\'' +
+                ", appointmentDescription='" + appointmentDescription + '\'' +
+                '}';
+    }
 }
--- src/main/java/com/team4/appointment_service/AppointmentController.java
+++ src/main/java/com/team4/appointment_service/AppointmentController.java
@@ -1,7 +1,40 @@
 package com.team4.appointment_service;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class AppointmentController {
+
+    private final AppointmentService appointmentService;
+
+    public AppointmentController(AppointmentService appointmentService) {
+        this.appointmentService = appointmentService;
+    }
+
+    //shows a specific appointment
+    @GetMapping(value = "/{id}")
+    public Appointment viewAppointmentById(@PathVariable Long id){
+        return appointmentService.findById(id);
+    }
+
+
+    //shows all appointments a patient has on record
+    @GetMapping(value = "/{patientId}")
+    public List<Appointment> viewAppointmentOfPatient(@PathVariable Long patientId){
+        return appointmentService.findByPatientId(patientId);
+    }
+
+
+    //shows all appointments a doctor has on record
+    @GetMapping(value = "/{doctorId}")
+    public List<Appointment> viewAppointmentOfDoctor(@PathVariable Long doctorId){
+        return appointmentService.findByDoctorId(doctorId);
+    }
+
+    @PostMapping("/deleteAppointment")
+    public void deleteAppointment(@RequestBody Long appointmentId) {
+        appointmentService.deleteAppointment(appointmentId);
+    }
 }
